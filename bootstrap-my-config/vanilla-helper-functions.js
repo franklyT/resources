7beb5e8aef27c349a4e63e7f7ebd4d9dd33553d5
franklyT <48483484+franklyT@users.noreply.github.com>
2019-08-15 15:14:32 -0400

    Update vanilla-helper-functions.js

--- bootstrap-my-config/vanilla-helper-functions.js
+++ bootstrap-my-config/vanilla-helper-functions.js
@@ -105,3 +105,26 @@ function isInViewport(elm) {
     && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
+
+function findDuplicateInArray(arra1) {
+        var object = {};
+        var result = [];
+
+        arra1.forEach(function (item) {
+          if(!object[item])
+              object[item] = 0;
+            object[item] += 1;
+        })
+
+        for (var prop in object) {
+           if(object[prop] >= 2) {
+               result.push(prop);
+           }
+        }
+
+        return result;
+
+    }
+
+    console.log(findDuplicateInArray([1, 1, 2, 3]));
+

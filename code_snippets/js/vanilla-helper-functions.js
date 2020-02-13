0c64c1cddfabdad080f661bf18b0b49918832d0a
franklyt <franklyt@users.noreply.github.com>
2020-02-12 22:37:57 -0500

    upd

--- code_snippets/js/vanilla-helper-functions.js
+++ code_snippets/js/vanilla-helper-functions.js
@@ -31,7 +31,7 @@ function onDOMLoad(method) {
 }
 
 // random inclusive
-getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive

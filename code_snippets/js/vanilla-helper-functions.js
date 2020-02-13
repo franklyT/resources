e946a8c5b25cae74b5c219131915eac4f0288281
franklyt <franklyt@users.noreply.github.com>
2020-02-12 22:35:42 -0500

    Update vanilla-helper-functions.js

--- code_snippets/js/vanilla-helper-functions.js
+++ code_snippets/js/vanilla-helper-functions.js
@@ -112,24 +112,24 @@ function isInViewport(elm) {
 }
 
 function findDuplicateInArray(arra1) {
-        var object = {};
-        var result = [];
+  var object = {};
+  var result = [];
 
-        arra1.forEach(function (item) {
-          if(!object[item])
-              object[item] = 0;
-            object[item] += 1;
-        })
+  arra1.forEach(function (item) {
+    if (!object[item])
+      object[item] = 0;
+    object[item] += 1;
+  })
 
-        for (var prop in object) {
-           if(object[prop] >= 2) {
-               result.push(prop);
-           }
-        }
-
-        return result;
-
+  for (var prop in object) {
+    if (object[prop] >= 2) {
+      result.push(prop);
     }
+  }
 
-    console.log(findDuplicateInArray([1, 1, 2, 3]));
+  return result;
+
+}
+
+console.log(findDuplicateInArray([1, 1, 2, 3]));
 
